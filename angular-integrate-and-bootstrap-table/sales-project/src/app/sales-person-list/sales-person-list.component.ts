e1b94b7451b6b3910af220a775ed753aca973300
SUMAN RANI <SUMANKAMBOJ1997@GMAIL.COM>
2025-05-19 20:34:27 -0400

    sales table by using bootstrapbootstrap

---
+++ angular-integrate-and-bootstrap-table/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list-bootstrap.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] = [
+   new SalesPerson("Suman","rani", "suman123@.com", 50000),
+    new SalesPerson("Jonii","sama", "jonii.sama@.com", 40000),
+    new SalesPerson("Aman","Sama", "aman.sama@.com", 90000),
+    new SalesPerson("Simran","Kamboj", "simran.kamaboj@.com", 60000),
+    new SalesPerson("Simar","Rani", "simar.rani@.com", 90000),
+    new SalesPerson("Anikat","sharama", "anikat@.com", 60000),
+      new SalesPerson("Nitish","Kumar", "nitish@.com", 40000),
+       new SalesPerson("Digna","Patel", "digna@.com", 50000),
+    
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
